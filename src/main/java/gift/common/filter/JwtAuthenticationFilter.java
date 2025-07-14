3c0c6ce41622f3e6a06ef9290cb17b14002531fe
javachip <79399385+thstkddnr20@users.noreply.github.com>
2025-07-14 21:59:43 +0900

    chore: 위시리스트 코드 이관 (#4)
    Co-authored-by: thstkddnr20 <thstkddnr20@gmail.com>

---
+++ src/main/java/gift/common/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,100 @@
+package gift.common.filter;
+
+import gift.common.exception.InvalidAccessTokenException;
+import gift.common.exception.InvalidTokenException;
+import gift.dto.user.UserInfo;
+import gift.service.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+@Order(2)
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String AUTH_HEADER = "Authorization";
+    private static final String HEADER_PREFIX = "Bearer ";
+    private static final String COOKIE_NAME = "accessToken";
+
+    private static final Map<String, Set<HttpMethod>> BLACKLIST = Map.of(
+            "/api/users/register", Set.of(HttpMethod.POST),
+            "/api/users/login", Set.of(HttpMethod.POST),
+            "/admin/login", Set.of(HttpMethod.GET, HttpMethod.POST),
+            "/api/products", Set.of(HttpMethod.GET)
+    );
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpMethod method = HttpMethod.valueOf(request.getMethod());
+        String requestURI = request.getRequestURI();
+
+        if (BLACKLIST.containsKey(requestURI)) {
+            Set<HttpMethod> methods = BLACKLIST.get(requestURI);
+            if (methods.contains(method)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+        }
+
+        try {
+            String jwt = null;
+            String authHeader = request.getHeader(AUTH_HEADER);
+            if (authHeader != null) {
+                authHeader = authHeader.trim();
+            }
+
+            if (authHeader != null && authHeader.startsWith(HEADER_PREFIX)) {
+                jwt = authHeader.substring(HEADER_PREFIX.length());
+            }
+
+            if (authHeader == null) {
+                if (request.getCookies().length == 0) {
+                    throw new InvalidAccessTokenException();
+                }
+                for (Cookie cookie : request.getCookies()) {
+                    if (cookie.getName().equals(COOKIE_NAME)) {
+                        String val = URLDecoder.decode(cookie.getValue(), StandardCharsets.UTF_8);
+                        if (!val.startsWith(HEADER_PREFIX)) {
+                            throw new InvalidTokenException();
+                        }
+                        jwt = val.substring(HEADER_PREFIX.length());
+                        break;
+                    }
+                }
+            }
+
+            if (jwt == null) {
+                throw new InvalidAccessTokenException();
+            }
+
+            Claims claims = jwtTokenProvider.validAccessToken(jwt);
+            UserInfo userInfo = jwtTokenProvider.getUserInfoFromClaims(claims);
+            request.setAttribute("userInfo", userInfo);
+
+        } catch (RuntimeException e) {
+            throw new InvalidTokenException(e);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/gift/controller/ProductManageController.java
@@ -0,0 +1,73 @@
+package gift.controller;
+
+import gift.common.interceptor.AdminOnly;
+import gift.dto.product.CreateProductRequest;
+import gift.dto.product.ProductManageResponse;
+import gift.dto.product.UpdateProductRequest;
+import gift.service.ProductManageService;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/products")
+@AdminOnly
+public class ProductManageController {
+
+    private final ProductManageService productManageService;
+
+    public ProductManageController(ProductManageService productManageService) {
+        this.productManageService = productManageService;
+    }
+
+    @GetMapping
+    public String getProductsForm(Model model) {
+        List<ProductManageResponse> products = productManageService.getAllProducts();
+        model.addAttribute("products", products);
+        return "/admin/product/productList";
+    }
+
+    @GetMapping("/new")
+    public String createProductForm(Model model) {
+        model.addAttribute("request", CreateProductRequest.empty());
+        return "/admin/product/productCreate";
+    }
+
+
+    @PostMapping
+    public String createProduct(@ModelAttribute(name = "request") @Valid CreateProductRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/admin/product/productCreate";
+        }
+        productManageService.saveProduct(request);
+        return "redirect:/admin/products";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateProductForm(@PathVariable Long id, Model model) {
+        ProductManageResponse response = productManageService.getProduct(id);
+        model.addAttribute("id", id);
+        model.addAttribute("request", UpdateProductRequest.from(response));
+        return "/admin/product/productUpdate";
+    }
+
+    @PostMapping("/{id}")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute(name = "request") @Valid UpdateProductRequest request, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/admin/product/productUpdate";
+        }
+        productManageService.updateProduct(id, request);
+        return "redirect:/admin/products";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String deleteProduct(@PathVariable Long id) {
+        productManageService.deleteProduct(id);
+        return "redirect:/admin/product/products";
+    }
+
+}
