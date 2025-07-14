ea603ab2b2d98c0fb9fa48071b81f79dcbc61fc3
sanghyeon1225 <oopt1234@naver.com>
2025-07-14 23:20:41 +0900

    feat: JPA 사용을 위한 Repository 인터페이스 작성

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -1,73 +1,17 @@
 package gift.repository;
 
-import gift.dto.WishRequestDto;
+import gift.entity.Member;
+import gift.entity.Product;
 import gift.entity.Wish;
 import java.util.List;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WishRepository {
-    private final JdbcClient jdbcClient;
+public interface WishRepository extends JpaRepository<Wish, Long> {
+    public List<Wish> findByMember(Member member);
 
-    public WishRepository(JdbcClient jdbcClient) {
-        this.jdbcClient = jdbcClient;
-    }
+    boolean existsByMemberAndProduct(Member member, Product product);
 
-    public List<Wish> findByMemberId(Long memberId) {
-        String sql = """
-            SELECT id, member_id, product_id FROM WISHLIST WHERE member_id = :id
-            """;
-        return jdbcClient.sql(sql)
-                .param("id", memberId)
-                .query(wishRowMapper())
-                .list();
-    }
-
-    public void addWish(Long memberId, Long productId) {
-        String sql = """
-                INSERT INTO WISHLIST (member_id, product_id) VALUES (:member_id, :product_id)
-                """;
-
-        jdbcClient.sql(sql)
-                .param("member_id", memberId)
-                .param("product_id", productId)
-                .update();
-    }
-
-    public void deleteWish(Long memberId, Long productId) {
-        String sql = """
-                DELETE FROM WISHLIST WHERE member_id = :member_id AND product_id = :product_id
-                """;
-
-        jdbcClient.sql(sql)
-                .param("member_id", memberId)
-                .param("product_id", productId)
-                .update();
-    }
-
-    public boolean existWish(Long memberId, Long productId) {
-        String sql = """
-                SELECT COUNT(*) FROM WISHLIST WHERE member_id = :member_id AND  product_id = :product_id
-                """;
-
-        Integer count = jdbcClient.sql(sql)
-                .param("member_id", memberId)
-                .param("product_id", productId)
-                .query(Integer.class)
-                .single();
-
-        return count > 0;
-    }
-
-    private RowMapper<Wish> wishRowMapper() {
-        return (rs, rowNum) -> new Wish(
-                rs.getLong("id"),
-                rs.getLong("member_id"),
-                rs.getLong("product_id")
-                );
-    }
+    void deleteByMemberAndProduct(Member member, Product product);
 }
