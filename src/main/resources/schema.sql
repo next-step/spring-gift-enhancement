2a4668f2ab55bf9fce567dd1a2b5f3fb36e1dd56
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-14 17:26:47 +0900

    feat: 미션2 코드 복붙 및 미션3 PR 준비

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+create table products
+(
+    id        bigint auto_increment primary key,
+    name      varchar(255) not null,
+    price     int          not null check (price >= 0),
+    image_url varchar(1000)
+);
+
+CREATE TABLE members (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL
+);
+
+create table if not exists wishes (
+    id bigint auto_increment primary key,
+    member_id bigint not null,
+    product_id bigint not null,
+    quantity int not null default 1
+);
+
