a9d2a64b95687d53f7e922b97f2f74ff3928a26d
seongwon02 <seongwon2303@gmail.com>
2025-07-15 17:45:03 +0900

    refactor: 테이블 명 변경에 따른 data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
-insert into products (name, price, imageUrl)
+insert into product (name, price, image_url)
 values ('사과 2kg', 14900, 'https://example1.png');
 
-insert into products (name, price, imageUrl)
+insert into product (name, price, image_url)
 values ('카카오프랜즈 인형', 30000, 'https://example2.png');
 
-insert into members (email, password, role)
+insert into member (email, password, role)
 values ('admin@example.com', '1234', 'ADMIN');
 
-insert into members (email, password, role)
+insert into member (email, password, role)
 values ('user@example.com', '0000', 'USER');
 
-insert into wishes (member_id, product_id)
+insert into wish (member_id, product_id)
 values (2, 1);
 
-insert into members (email, password, role)
+insert into member (email, password, role)
 values ('user2@example.com', '0000', 'USER');
 
-insert into wishes (member_id, product_id)
+insert into wish (member_id, product_id)
 values (3, 2);
