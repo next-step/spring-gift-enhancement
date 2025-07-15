83706cfdcfa143ef11c8b9b8ccf8490726bbbdea
jihwan38 <sss203800@gmail.com>
2025-07-15 12:26:34 +0900

    feat: DataJpaTest를 이용한 ProductRepository 테스트 코드 추가

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 ## 1단계 - 엔티티 매핑
 ### 기능 목록
 - [x] JPA 의존성 추가 및 application.properties에 관련 설정 추가
-- [ ] Product 리팩토링
+- [x] Product 리팩토링
   - [x] Product를 JPA 엔티티로 변환
   - [x] ProductRepository를 Jdbc 기반에서 JPA 기반으로 변경 
   - [x] ProductRepository에 의존하는 WishService 및 WishServiceTest 리팩토링
   - [x] JPA 적용으로 인한 ProductService 로직 변경
+  - [x] DataJpaTest를 이용한 ProductRepository 테스트 코드 추가
---
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -0,0 +1,88 @@
+package gift;
+
+
+
+import gift.product.entity.Product;
+import gift.product.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+
+@DataJpaTest
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    void 상품_저장(){
+        var product = new Product("새로운 상품", 1000L, "http://image.url", null);
+        assertThat(product.getId()).isNull();
+        var actual = productRepository.save(product);
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getName()).isEqualTo("새로운 상품");
+        assertThat(actual.getPrice()).isEqualTo(1000L);
+        assertThat(actual.getImageUrl()).isEqualTo("http://image.url");
+        assertThat(actual.getIsKakaoApprovedByMd()).isEqualTo(false);
+    }
+
+    @Test
+    void 상품조회(){
+        var product = new Product("새로운 상품", 1000L, "http://image.url", null);
+        var saved = productRepository.save(product);
+        var actual = productRepository.findById(saved.getId()).get();
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getName()).isEqualTo("새로운 상품");
+        assertThat(actual.getPrice()).isEqualTo(1000L);
+        assertThat(actual.getImageUrl()).isEqualTo("http://image.url");
+        assertThat(actual.getIsKakaoApprovedByMd()).isEqualTo(false);
+    }
+
+    @Test
+    void 모든_상품조회(){
+       productRepository.save(new Product("상품1", 1000L, "http://image.url", null));
+       productRepository.save(new Product("상품2", 2000L, "http://image.url", null));
+       productRepository.save(new Product("상품3", 3000L, "http://image.url", null));
+
+       var actual = productRepository.findAll();
+
+       assertThat(actual.size()).isEqualTo(3);
+       assertThat(actual).extracting(Product::getName)
+               .containsExactlyInAnyOrder("상품1", "상품2", "상품3");
+    }
+
+    @Test
+    void 상품수정(){
+        var saved = productRepository.save(new Product("상품1", 1000L, "http://image.url", null));
+
+        var product = productRepository.findById(saved.getId()).get();
+        product.updateProduct("카카오상품", 2000L, "http://update_image.url", true);
+        entityManager.flush();
+
+        var actual = productRepository.findById(saved.getId()).get();
+        assertThat(actual.getId()).isNotNull();
+        assertThat(actual.getName()).isEqualTo("카카오상품");
+        assertThat(actual.getPrice()).isEqualTo(2000L);
+        assertThat(actual.getImageUrl()).isEqualTo("http://update_image.url");
+        assertThat(actual.getIsKakaoApprovedByMd()).isEqualTo(true);
+    }
+
+    @Test
+    void 상품삭제(){
+        var saved = productRepository.save(new Product("상품1", 1000L, "http://image.url", null));
+
+        productRepository.deleteById(saved.getId());
+
+        var actual = productRepository.findById(saved.getId());
+
+        assertThat(actual).isEmpty();
+    }
+
+}
