88e5443a02b91745a358b2a04e00984f103e8a86
raminKim <151427064+raminkim@users.noreply.github.com>
2025-07-15 18:43:38 +0900

    Test: MemberRepository JPA findById 테스트 코드 수정

--- src/test/java/gift/jpa/MemberRepositoryTest.java
+++ src/test/java/gift/jpa/MemberRepositoryTest.java
@@ -32,8 +32,12 @@ public class MemberRepositoryTest {
 
     @Test
     void findById() {
+        Member expected = new Member("example@naver.com", "f6f2ea8f45d8a057c9566a33f99474da2e5c6a6604d736121650e2730c6fb0a3");
         Member actual = memberRepository.findById(1L).get();
-        assertThat(actual.getId()).isEqualTo(1L);
+        assertAll(
+            () -> assertThat(actual.getEmail()).isEqualTo(expected.getEmail()),
+            () -> assertThat(actual.getPassword()).isEqualTo(expected.getPassword())
+        );
     }
 
     @Test
