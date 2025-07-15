1a065ebc91fbb3272b6d6d9a5f537044f1e5ac69
gosurim <su020900@gmail.com>
2025-07-15 18:24:29 +0900

    refactor: Product 도메인 JPA 전환

---
+++ src/main/java/giftproject/gift/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package giftproject.gift.repository;
+
+import giftproject.gift.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
