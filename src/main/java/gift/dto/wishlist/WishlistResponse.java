354987f5a6c0dd9b50f242877f4dee47fc0552d3
thstkddnr20 <thstkddnr20@gmail.com>
2025-07-15 14:56:31 +0900

    fix: WishlistResponse 반환을 위해 jpql 작성

--- src/main/java/gift/dto/wishlist/WishlistResponse.java
+++ src/main/java/gift/dto/wishlist/WishlistResponse.java
@@ -1,4 +1,29 @@
 package gift.dto.wishlist;
 
-public record WishlistResponse(Long productId, String productName, String productPrice) {
+import gift.domain.Product;
+import gift.domain.Wishlist;
+
+public class WishlistResponse {
+    Long productId;
+    String productName;
+    Integer productPrice;
+
+    public WishlistResponse(Wishlist wishlist) {
+        Product product = wishlist.getProduct();
+        this.productId = product.getId();
+        this.productName = product.getName();
+        this.productPrice = product.getPrice();
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Integer getProductPrice() {
+        return productPrice;
+    }
 }
--- src/main/java/gift/repository/WishlistRepository.java
+++ src/main/java/gift/repository/WishlistRepository.java
@@ -3,6 +3,7 @@ package gift.repository;
 import gift.domain.Wishlist;
 import gift.dto.wishlist.WishlistResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,6 @@ public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
 
     Optional<Wishlist> findByProductId(Long productId);
 
+    @Query("select new gift.dto.wishlist.WishlistResponse(w) from Wishlist w where w.user.id=:userId")
     List<WishlistResponse> findAllByUserId(Long userId);
 }
