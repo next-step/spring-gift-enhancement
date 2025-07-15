daefd17adb3401a592648a8434818379f26fea9d
xodbs126 <dltvmsskd@naver.com>
2025-07-15 17:55:42 +0900

    refactor(wish): WishItem 클래스로 변환

--- src/main/java/gift/entity/WishItem.java
+++ src/main/java/gift/entity/WishItem.java
@@ -1,4 +1,59 @@
 package gift.entity;
 
-public record WishItem(Long id, Long userId,Long itemId , Integer quantity) {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "wish")
+public class WishItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id")
+    private User user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "product_id")
+    private Item item;
+
+    @NotNull
+    @Min(1)
+    @Column(nullable = false)
+    private Integer quantity;
+
+    protected WishItem() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
