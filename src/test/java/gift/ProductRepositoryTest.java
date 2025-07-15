b7964b12bec5a5eaeb97283f57140c4618d2115c
oh010 <oh010224@gmail.com>
2025-07-15 16:14:47 +0900

    feat: DataJpaTest 추가

---
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -0,0 +1,30 @@
+package gift;
+
+import gift.product.domain.Product;
+import gift.product.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataJpaTest
+@EntityScan(basePackages = "gift")
+public class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void save() {
+        Product expected = new Product("선풍기", 1000, "");
+        Product actual = productRepository.save(expected);
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+                () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice()),
+                () -> assertThat(actual.getImageUrl()).isEqualTo(expected.getImageUrl())
+        );
+    }
+}
