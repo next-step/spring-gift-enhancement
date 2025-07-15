eb81dfc1da0524018b68adc2d1026b31ee57ac2c
gosurim <su020900@gmail.com>
2025-07-15 18:24:52 +0900

    refactor: Member 도메인 JPA 전환

---
+++ src/main/java/giftproject/member/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package giftproject.member.repository;
+
+import giftproject.member.entity.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}
