fd5a119cb1b5daa74bb154b061fbff89ea7d05de
uvrvuoon <0524yar@gmail.com>
2025-07-15 16:44:47 +0900

    feat: 기존 Jdbc로 구현했던 Repository들을 JPA로 리팩토링

--- src/main/java/gift/repository/ProductRepository.java
+++ src/main/java/gift/repository/ProductRepository.java
@@ -1,23 +1,8 @@
 package gift.repository;
 
 import gift.entity.Product;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // 1. 저장
-    Product save(Product product);
-
-    // 2-1. 전체 조회
-    List<Product> findAll();
-
-    // 2-2. 선택 조회
-    Optional<Product> findById(Long id);
-
-    // 3. 수정
-    void update(Product product);
-
-    // 4. 삭제
-    void deleteById(Long id);
 }
