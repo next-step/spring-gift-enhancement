16cce8b8af1929bc6ef12db1c289ab34b462313c
sylimi2r2 <108124934+sylimi2r2@users.noreply.github.com>
2025-07-17 00:08:51 +0900

    부산대 BE_임석윤 1단계 - 엔티티 매핑 (#165)
    * docs: 기능 요구사항 정리
    
    * fix: db schema 수정
    
    * refactor: product 관련 서비스 JPA 리팩토링
    
    * refactor: user 관련 서비스 JPA 리팩토링
    
    * refactor: wish 관련 서비스 JPA 리팩토링
    
    * feat: exception handler 예외 추가

--- src/main/java/gift/repository/H2UserRepository.java
+++ src/main/java/gift/repository/H2UserRepository.java
@@ -1,17 +1,9 @@
 package gift.repository;
 
-import gift.entity.User;
-import gift.entity.vo.Email;
-import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
+@Deprecated
 public class H2UserRepository {
 
+    /*
     private final JdbcClient jdbcClient;
 
     public H2UserRepository(JdbcClient jdbcClient) {
@@ -42,4 +34,5 @@ public class H2UserRepository {
         user.setId(keyHolder.getKey().longValue());
         return user;
     }
+    */
 }
