8d409c11f8ec31f148b02cc5f50516b3f377db89
seongwon02 <seongwon2303@gmail.com>
2025-07-16 15:42:47 +0900

    refactor: 멤버 상위 예외 클래스 생성 및 기존 예외 클래스 로직 수정

--- src/main/java/gift/exception/member/MemberNotFoundException.java
+++ src/main/java/gift/exception/member/MemberNotFoundException.java
@@ -1,7 +1,9 @@
 package gift.exception.member;
 
-public class MemberNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+public class MemberNotFoundException extends MemberException {
     public MemberNotFoundException(String message) {
-        super(message);
+        super(message, HttpStatus.NOT_FOUND);
     }
 }
