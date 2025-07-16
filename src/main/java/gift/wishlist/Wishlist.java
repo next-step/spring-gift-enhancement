3a2dcd69c77b0c893999e6228b5d84572fb4f030
oh010 <oh010224@gmail.com>
2025-07-16 15:26:53 +0900

    refactor: 컬럼명 명시

--- src/main/java/gift/wishlist/Wishlist.java
+++ src/main/java/gift/wishlist/Wishlist.java
@@ -12,9 +12,11 @@ public class Wishlist {
     private Long id;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
     private Product product;
 
     protected Wishlist() {}
