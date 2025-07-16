2027d3d024ab6da491052ae80950c0bc239e07a3
oh010 <oh010224@gmail.com>
2025-07-16 16:07:35 +0900

    refactor: 프로퍼티 파일 분리

--- src/test/java/gift/ProductRepositoryTest.java
+++ src/test/java/gift/ProductRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
