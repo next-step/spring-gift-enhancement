ae86fe9722577123453cd5fbc653f1e7b958f89f
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-17 14:15:58 +0900

    refactor: 불필요한 setter 제거

--- src/main/java/gift/member/entity/Member.java
+++ src/main/java/gift/member/entity/Member.java
@@ -46,16 +46,4 @@ public class Member {
     public void setId(Long id) {
         this.id = id;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
