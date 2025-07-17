acc590579427ca0b498b69454ef65ea0cd6b4b10
최효은 <choehyoeun@choehyoeun-ui-MacBookAir.local>
2025-07-17 17:31:31 +0900

    test: 전체 시나리오 테스트 추가

--- src/test/java/gift/WishRepositoryTest.java
+++ src/test/java/gift/WishRepositoryTest.java
@@ -11,8 +11,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -51,11 +55,19 @@ class WishRepositoryTest {
     @Test
     @DisplayName("회원별 위시리스트 조회")
     void findByMember() {
+        // given
+        Member member = memberRepository.save(new Member("user1@test.com", "pw", gift.entity.MemberRole.USER));
+        Product product = productRepository.save(new Product("테스트상품", 1000, "img.jpg"));
         Wish wish = new Wish(member, product, 2);
         wishRepository.save(wish);
 
-        List<Wish> list = wishRepository.findByMemberOrderByIdDesc(member);
+        Pageable pageable = PageRequest.of(0, 10);
 
+        // when
+        Page<Wish> page = wishRepository.findByMember(member, pageable);
+        List<Wish> list = page.getContent();
+
+        // then
         assertThat(list).hasSize(1);
         assertThat(list.get(0).getProduct().getName()).isEqualTo("테스트상품");
     }
