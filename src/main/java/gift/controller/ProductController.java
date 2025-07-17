71a27d73a174d9336395f31068366a506c8c0e25
uvrvuoon <0524yar@gmail.com>
2025-07-17 16:07:41 +0900

    feat: 상품 컨트롤러에서 페이지네이션 구현

--- src/main/java/gift/controller/ProductController.java
+++ src/main/java/gift/controller/ProductController.java
@@ -4,6 +4,8 @@ import gift.dto.RequestDto;
 import gift.dto.ResponseDto;
 import gift.service.ProductService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,7 +37,21 @@ public class ProductController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    // 2. 상품 조회
+    // 2-1. 상품 전체 조회
+    @GetMapping
+    public ResponseEntity<Page<ResponseDto>> getProducts(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        Page<ResponseDto> productPage = productService.findAll(page, size);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Page-Number", String.valueOf((productPage.getNumber())));
+        headers.add("X-Page-Size", String.valueOf(productPage.getSize()));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(productPage);
+    }
+
+    // 2-2. 상품 단건 조회
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDto> getProduct(@PathVariable Long id) {
         ResponseDto response = productService.findById(id);
