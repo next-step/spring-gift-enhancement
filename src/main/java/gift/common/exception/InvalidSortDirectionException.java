d098ddbf8b6c90e53c2eccc98d2084cf334b5a32
treasure-sky <ych0486666@gmail.com>
2025-07-17 17:32:23 +0900

    feat: 예외 메세지 가독성 향상

--- src/main/java/gift/common/exception/InvalidSortDirectionException.java
+++ src/main/java/gift/common/exception/InvalidSortDirectionException.java
@@ -3,6 +3,6 @@ package gift.common.exception;
 public class InvalidSortDirectionException extends RuntimeException {
 
     public InvalidSortDirectionException(String direction) {
-        super("올바른 정렬 방향이 아닙니다: " + direction + "asc, desc 중 하나가 필요합니다.");
+        super("올바른 정렬 방향이 아닙니다: " + direction + ", asc / desc 중 하나가 필요합니다.");
     }
 }
