f7b69762a1c0a83c043896cb95525b87f4cc13f3
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-17 14:10:56 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/gift/member/repository/MemberRepository.java
+++ src/main/java/gift/member/repository/MemberRepository.java
@@ -1,20 +1,9 @@
 package gift.member.repository;
 
-import gift.member.dto.MemberRequestDto;
 import gift.member.entity.Member;
-import gift.product.dto.ProductResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Optional;
 
 @Repository
--- src/main/java/gift/wish/repository/WishRepository.java
+++ src/main/java/gift/wish/repository/WishRepository.java
@@ -1,19 +1,9 @@
 package gift.wish.repository;
 
-import gift.product.entity.Product;
-import gift.wish.dto.WishResponseDto;
 import gift.wish.entity.Wish;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
