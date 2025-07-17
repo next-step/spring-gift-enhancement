e85a0dd030041d68cd290f107414fe3632ff7fe9
hayun <psh040826@naver.com>
2025-07-18 03:57:07 +0900

    feat : Repository 페이지네이션 구현

--- src/main/java/gift/repository/WishRepository.java
+++ src/main/java/gift/repository/WishRepository.java
@@ -3,12 +3,18 @@ package gift.repository;
 import gift.entity.Member;
 import gift.entity.Product;
 import gift.entity.Wish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.Optional;
 
 public interface WishRepository extends JpaRepository<Wish, Long> {
-    List<Wish> findByMember(Member member);
+    @Query(value = "SELECT w FROM Wish w JOIN FETCH w.product p WHERE w.member = :member",
+            countQuery = "SELECT count(w) FROM Wish w WHERE w.member = :member")
+    Page<Wish> findByMemberWithProduct(@Param("member") Member member, Pageable pageable);
     Optional<Wish> findByMemberAndProduct(Member member, Product product);
     void deleteByMemberAndProduct(Member member, Product product);
 }
