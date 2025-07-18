c5db4b0742d5e769bcff0aad30378bea06537181
raminKim <151427064+raminkim@users.noreply.github.com>
2025-07-18 18:51:56 +0900

    Fix: JPQL fetch join을 사용해 N+1 문제 해결

--- src/main/java/gift/repository/wishlist/WishListRepository.java
+++ src/main/java/gift/repository/wishlist/WishListRepository.java
@@ -2,6 +2,7 @@ package gift.repository.wishlist;
 
 import gift.entity.Wish;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 public interface WishListRepository extends JpaRepository<Wish, Long> {
     Wish findByProductIdAndMemberId(Long productId, Long memberId);
 
-    List<Wish> findAllByMemberId(Long memberId);
+    @Query("select w from Wish w join fetch w.product join fetch w.member where w.member.id = :memberId")
+    List<Wish> findAllByMemberId(@Param("memberId") Long memberId);
 
     @Transactional
     int deleteByProductIdAndMemberId(Long productId, Long memberId);
