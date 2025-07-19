1e7844d59cc3880d53d25a258024cc4baf536f4e
박신영 <park_new0@naver.com>
2025-07-19 10:14:35 +0900

    feat(view): 위시리스트 뷰 추가 및 홈 메뉴 개편
    사용자별 위시리스트 기능의 기반이 되는 UI를 추가하고, 홈페이지의 사용성을 개선

--- src/main/java/gift/controller/HomeController.java
+++ src/main/java/gift/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/wishes")
+    public String wishlist() {
+        return "user/wishlist";
+    }
 }
