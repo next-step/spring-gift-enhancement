5f3a99befaf72aea03ec684a800cc4ecfda219e4
uvrvuoon <0524yar@gmail.com>
2025-07-21 00:11:38 +0900

    feat: ProductRequestDto 필드의 불변성 제거 (AdminProductController에서의 오류 해결 위함)

--- src/main/java/gift/dto/ProductRequestDto.java
+++ src/main/java/gift/dto/ProductRequestDto.java
@@ -6,25 +6,20 @@ import jakarta.validation.constraints.Size;
 
 public class ProductRequestDto {
 
-    private final Long id;
+    private Long id;
 
     @NotNull
     @Size(max = 15, message = "공백 포함 최대 15자까지만 입력 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다.")
-    private final String name;
+    private String name;
 
     @NotNull
-    private final String imageUrl;
+    private String imageUrl;
 
     @NotNull
-    private final Long price;
+    private Long price;
 
-    public ProductRequestDto(Long id, String name, String imageUrl, Long price) {
-        this.id = id;
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.price = price;
-    }
+    public ProductRequestDto() {}
 
     public Long getId() {
         return id;
