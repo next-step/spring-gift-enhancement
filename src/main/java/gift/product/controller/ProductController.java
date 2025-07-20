12a70fdd72d03e67c4d75c19b6d9df5b40409945
joongsunYoon <119833131+joongsunYoon@users.noreply.github.com>
2025-07-20 20:51:31 +0900

    refactor: 조회기능의 리턴 타입 pageDto로 변환

--- src/main/java/gift/product/controller/ProductController.java
+++ src/main/java/gift/product/controller/ProductController.java
@@ -1,6 +1,7 @@
 package gift.product.controller;
 
 import gift.exception.GlobalExceptionHandler.ApiResponse;
+import gift.product.dto.ProductPageDto;
 import gift.product.dto.ProductRequestDto;
 import gift.product.dto.ProductResponseDto;
 import gift.product.service.ProductService;
@@ -10,6 +11,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static gift.product.dto.ProductPageDto.fromEntity;
 
 @RestController
 @RequestMapping("/api/products")
@@ -30,9 +33,9 @@ public class ProductController {
 
     //전체 조회
     @GetMapping
-    public ResponseEntity<Page<ProductResponseDto>> getAllProducts(@PageableDefault(size = 5, sort = "name") Pageable pageable) {
+    public ResponseEntity<ProductPageDto<ProductResponseDto>> getAllProducts(@PageableDefault(size = 5, sort = "name") Pageable pageable) {
         Page<ProductResponseDto> productResponseDtos = productService.getAllProducts(pageable);
-        return  ResponseEntity.ok(productResponseDtos);
+        return  ResponseEntity.ok(fromEntity(productResponseDtos));
     }
 
     //특정 상품 추가
