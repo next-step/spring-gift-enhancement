6f33d094e4107bbec8ffde397d8e1ef02918180a
treasure-sky <ych0486666@gmail.com>
2025-07-21 02:26:45 +0900

    feat: OptionEntity 구현

---
+++ src/main/java/gift/item/OptionEntity.java
@@ -0,0 +1,27 @@
+package gift.item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "option")
+public class OptionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    protected OptionEntity() {
+    }
+
+}
