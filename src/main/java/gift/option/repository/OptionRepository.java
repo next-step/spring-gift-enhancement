12878e0638104578af65a648289235c06125687e
jihwan38 <sss203800@gmail.com>
2025-07-21 09:00:59 +0900

    fix: OptionRepository을 <Option, Long>으로 변경

--- src/main/java/gift/option/repository/OptionRepository.java
+++ src/main/java/gift/option/repository/OptionRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OptionRepository extends JpaRepository<Option, Integer> {}
+public interface OptionRepository extends JpaRepository<Option, Long> {}
