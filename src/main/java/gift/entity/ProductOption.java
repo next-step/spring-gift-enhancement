47f59e3f2906f0c59c1bfc34ac6bbaac69356bce
thdgmlwns1 <xhrl233@naver.com>
2025-07-21 12:52:44 +0900

    Feat: ProductOption entity 생성

---
+++ src/main/java/gift/entity/ProductOption.java
@@ -0,0 +1,24 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProductOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    protected ProductOption() {}
+
+
+}
