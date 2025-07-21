6c4dc3d9537b31d94c584f02b6545c4dcf40766b
sinsehwan <sk124590@gmail.com>
2025-07-21 14:11:03 +0900

    feat(product): add productOption & establish relationship between product and productOption

--- src/main/java/gift/product/domain/Product.java
+++ src/main/java/gift/product/domain/Product.java
@@ -1,11 +1,17 @@
 package gift.product.domain;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Product {
@@ -18,6 +24,9 @@ public class Product {
     private int price;
     @Column(nullable = false)
     private String imageUrl;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProductOption> productOptions = new ArrayList<>();
 
     protected Product() {}
 
@@ -36,6 +45,31 @@ public class Product {
         this(id, null, 0, null);
     }
 
+    public void addProductOption(ProductOption productOption){
+        if(productOption != null && !this.productOptions.contains(productOption)){
+            validateIsDuplicate(productOption);
+
+            this.productOptions.add(productOption);
+            productOption.setProduct(this);
+        }
+    }
+
+    private void validateIsDuplicate(ProductOption newOption){
+        boolean isDuplicate = productOptions
+                .stream()
+                .anyMatch(option -> Objects.equals(option.getName(), newOption.getName()));
+
+        if(isDuplicate){
+            throw new IllegalArgumentException("동일한 이름의 옵션이 이미 존재합니다.");
+        }
+    }
+
+    public void updateProduct(String name, int price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
     public Long getId(){
         return id;
     }
@@ -52,9 +86,7 @@ public class Product {
         return imageUrl;
     }
 
-    public void updateProduct(String name, int price, String imageUrl) {
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
+    public List<ProductOption> getProductOptions() {
+        return productOptions;
     }
 }
