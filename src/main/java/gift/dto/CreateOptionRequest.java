9f276c711f28add1fd930f943e31c8fd3a376ed5
uvrvuoon <0524yar@gmail.com>
2025-07-21 18:05:16 +0900

    feat: 옵션 요청 dto를 record 클래스로 변경

--- src/main/java/gift/dto/CreateOptionRequest.java
+++ src/main/java/gift/dto/CreateOptionRequest.java
@@ -4,34 +4,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
-public class CreateOptionRequest {
-
+public record CreateOptionRequest (
     @NotNull
     @Size(max = 50, message = "공백 포함 최대 50자까지만 입력 가능합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$", message = "특수문자는 (), [], +, -, &, /, _ 만 가능합니다.")
-    private final String name;
+    String name,
 
     @NotNull
-    private final int quantity;
+    int quantity,
 
-    private final Long productId;
-
-
-    public CreateOptionRequest(String name, int quantity, Long productId) {
-        this.name = name;
-        this.quantity = quantity;
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-}
+    Long productId
+) {}
