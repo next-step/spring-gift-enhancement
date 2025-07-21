f70353b514380719ddc042288176fac82c7fc3ff
hayun <psh040826@naver.com>
2025-07-21 23:56:46 +0900

    feat : DTO option 기능에 맞춰 수정

--- src/main/java/gift/dto/ProductRequest.java
+++ src/main/java/gift/dto/ProductRequest.java
@@ -3,18 +3,18 @@ package gift.dto;
 import gift.entity.Product;
 import gift.entity.vo.Money;
 import gift.entity.vo.ProductName;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.PositiveOrZero;
+import java.util.List;
 
 public record ProductRequest(
-        @NotBlank(message = "상품 이름은 비워둘 수 없습니다.")
-        String name,
-
-        @PositiveOrZero(message = "가격은 0 이상이어야 합니다.")
-        Integer price,
-
-        @NotBlank(message = "이미지 URL은 비워둘 수 없습니다.")
-        String imageUrl
+        @NotBlank String name,
+        @PositiveOrZero Integer price,
+        @NotBlank String imageUrl,
+        @NotEmpty(message = "상품에는 최소 하나 이상의 옵션이 있어야 합니다.")
+        @Valid List<OptionRequest> options
 ) {
     public Product toEntity() {
         return new Product(new ProductName(name), new Money(price), imageUrl);
