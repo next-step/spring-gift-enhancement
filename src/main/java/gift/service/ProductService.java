b7c6c5651aa5300af8689b31ddc30ee5f6223755
하석현 <haseokhyeon@haseoghyeons-MacBook-Air.local>
2025-07-21 17:14:29 +0900

    페이지네이션 추가

--- src/main/java/gift/service/ProductService.java
+++ src/main/java/gift/service/ProductService.java
@@ -2,6 +2,8 @@ package gift.service;
 
 import gift.domain.Product;
 import gift.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -52,4 +54,9 @@ public class ProductService {
             throw new IllegalArgumentException("상품명에 '카카오'가 포함되어 있습니다. 담당자 확인이 필요합니다.");
         }
     }
+
+    public Page<Product> getPagedProducts(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
 }
