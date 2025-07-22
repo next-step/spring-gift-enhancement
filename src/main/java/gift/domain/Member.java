4e95037370499e593d4ffcba58101c1f1da5cec1
KNU-K <knukang334@gmail.com>
2025-07-23 05:18:34 +0900

    feat: Member 엔티티에 JPA 어노테이션 추가

--- src/main/java/gift/domain/Member.java
+++ src/main/java/gift/domain/Member.java
@@ -5,10 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "member")
@@ -24,10 +21,8 @@ public class Member {
     @Column(nullable = false)
     private String password;
 
-    @OneToMany(mappedBy = "member")
-    private List<Wish> wishes = new ArrayList<>();
-
     protected Member() {
+        // JPA 기본 생성자
     }
 
     private Member(Long id, String email, String password) {
@@ -55,10 +50,6 @@ public class Member {
 
     public String password() {
         return password;
-    }
-
-    public List<Wish> getWishes() {
-        return wishes;
     }
 
     public void assignId(Long id) {
