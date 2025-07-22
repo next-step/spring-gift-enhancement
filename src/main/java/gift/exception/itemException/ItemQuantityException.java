ee22ee53bab61be590d1468cac319dc0685ab2ef
xodbs126 <dltvmsskd@naver.com>
2025-07-22 20:58:27 +0900

    refactor(option): 예외 ì² 처리 로직 수정

--- src/main/java/gift/exception/itemException/ItemQuantityException.java
+++ src/main/java/gift/exception/itemException/ItemQuantityException.java
@@ -3,8 +3,9 @@ package gift.exception.itemException;
 import gift.exception.ApplicationException;
 import org.springframework.http.HttpStatus;
 
-public class ItemQuantityException extends ApplicationException {
+public class ItemQuantityException extends RuntimeException {
+
     public ItemQuantityException() {
-        super(HttpStatus.BAD_REQUEST,"수량은 1 - 100,000,000 개까지 입니다.");
+        super("수량은 1 - 10,000,000개 까지입니다.");
     }
 }
