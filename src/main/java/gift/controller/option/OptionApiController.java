c73503f57f8ba71d21c4ccfd9ec9591bc958478d
Hyun <hyun_7586@naver.com>
2025-07-22 08:09:29 +0800

    feat: Option subtract 메서드 추가

--- src/main/java/gift/controller/option/OptionApiController.java
+++ src/main/java/gift/controller/option/OptionApiController.java
@@ -1,11 +1,15 @@
 package gift.controller.option;
 
+import gift.dto.option.OptionRequest;
 import gift.dto.option.OptionResponse;
+import gift.dto.option.OptionSubtractRequest;
 import gift.service.option.OptionService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +23,20 @@ public class OptionApiController {
     }
 
     // option 조회 메서드: 이후 관리자 기능으로 전환 예정
-    @GetMapping("/{option_id}")
+    @GetMapping("/{optionId}")
     public ResponseEntity<OptionResponse> getOptionById(
-        @PathVariable(name = "option_id") Long optionId
+        @PathVariable(name = "optionId") Long optionId
     ){
         return ResponseEntity.status(HttpStatus.OK)
             .body(optionService.getOptionById(optionId));
     }
 
+    // option subtract 메서드: 이후 관리자 기능으로 전환 예정
+    @PostMapping("/{optionId}")
+    public ResponseEntity<OptionResponse> subtractOption(
+        @RequestBody OptionSubtractRequest optionSubtractRequest
+    ){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(optionService.subtractOption(optionSubtractRequest));
+    }
 }
