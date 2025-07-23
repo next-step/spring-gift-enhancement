db17e35809be92f8917cb27dd9422dabaafbe375
wrd1stProgrammer <kicoa24@gmail.com>
2025-07-23 14:53:13 +0900

    feat : 피드백 주신 대로 OptionResponse를 class에서 record로 변경했습니다.

--- src/main/java/gift/dto/OptionResponse.java
+++ src/main/java/gift/dto/OptionResponse.java
@@ -2,31 +2,8 @@ package gift.dto;
 
 import gift.domain.Option;
 
-public class OptionResponse {
-
-    private final Long id;
-    private final String name;
-    private final int quantity;
-
-    public OptionResponse(Long id, String name, int quantity) {
-        this.id = id;
-        this.name = name;
-        this.quantity = quantity;
-    }
-
+public record OptionResponse(Long id, String name, int quantity) {
     public static OptionResponse from(Option option) {
         return new OptionResponse(option.getId(), option.getName(), option.getQuantity());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getQuantity() {
-        return quantity;
     }
 }
